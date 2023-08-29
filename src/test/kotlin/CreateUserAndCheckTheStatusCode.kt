96091d7163c860c5cd3091b65e2a2ef733ff233f
ivnviv <vanya0774@gmail.com>
2023-08-29 21:09:45 +0300

    tests for creating users are done

--- src/test/kotlin/CreateUserAndCheckTheStatusCode.kt
+++ src/test/kotlin/CreateUserAndCheckTheStatusCode.kt
@@ -5,21 +5,26 @@ import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import khttp.post
 import khttp.get
+import java.util.*
 
 class CreateUserAndCheckTheStatusCode : StringSpec() {
-    private val createUserUrl = "https://petstore.swagger.io/v2/user"
-    private var createdUserId: Long = 0
+
+    private val createUserUrl =
+        "https://petstore.swagger.io/v2/user" // url для отправки запроса на получение данных о юзере
+
+    val userNameToCheck = UUID.randomUUID().toString() // Генерация случайного имени
+    private var createdUserId: Long = 0 // сюда будет записываться id созданного юзера для сравнения
+    val data = mapOf(
+        "username" to "$userNameToCheck",
+        "firstName" to "Kotlin",
+        "lastName" to "Kotlinov",
+        "email" to "kotlin@yandex.ru",
+        "password" to "kotlin",
+        "phone" to "89999999999"
+    )
 
     init {
         "it is possible to create user" {
-            val data = mapOf(
-                "username" to "KotlinTestUser",
-                "firstName" to "Kotlin",
-                "lastName" to "Kotlinov",
-                "email" to "kotlin@yandex.ru",
-                "password" to "kotlin",
-                "phone" to "89999999999"
-            )
             val createUserResponse = post(createUserUrl, json = data)
             createUserResponse.statusCode shouldBe 200
 
@@ -27,12 +32,13 @@ class CreateUserAndCheckTheStatusCode : StringSpec() {
         }
 
         "check created user in get request" {
-            //val getUserResponse = get("$createUserUrl/$createdUserId")
-            val getUserResponse = get("https://petstore.swagger.io/v2/user/KotlinTestUser")
+            val getUserResponse = get("$createUserUrl/$userNameToCheck")
             getUserResponse.statusCode shouldBe 200
-            "user exists and username is correct" {
-                getUserResponse.jsonObject.getString("username") shouldBe "KotlinTestUser"
-            }
+
+            getUserResponse.jsonObject.getString("username") shouldBe userNameToCheck
+            getUserResponse.jsonObject.getLong("id") shouldBe createdUserId
+
+
         }
     }
 }
